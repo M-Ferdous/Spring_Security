91130e2709e58ad6d38258e9a38732d5dc1a9043
M-Ferdous <mahzabin.courses@gmail.com>
2025-04-23 15:59:18 +0600

    v1

---
+++ src/main/java/com/example/springSecurity/GreetingsController.java
@@ -0,0 +1,13 @@
+package com.example.springSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class GreetingsController {
+    @GetMapping("/start")
+    public String greet() {
+        return "bismillah,   alhamdulillah";
+    }
+}
