9d29124f1e96a3483a07fb9391f3769f1ad3e5de
M-Ferdous <mahzabin.courses@gmail.com>
2025-04-24 11:51:33 +0600

    adding new features & previous ignored files

--- src/main/java/com/example/springSecurity/GreetingsController.java
+++ src/main/java/com/example/springSecurity/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.springSecurity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingsController {
     @GetMapping("/start")
     public String greet() {
-        return "sn .................. tesing";
+        return "sn..learning spring security .... tesing";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint() {
+        return "user end point on work";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint() {
+        return "admin end point is working";
     }
 }
--- src/main/java/com/example/springSecurity/SecurityConfig.java
+++ src/main/java/com/example/springSecurity/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.springSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -11,34 +13,62 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(auth -> auth
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
-
+        http.headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-       UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{noop}passwordAdmin")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user1, admin);
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        if (!userDetailsManager.userExists("user1")) {
+            userDetailsManager.createUser(
+                    User.withUsername("user1")
+                            .password(passwordEncoder().encode("password23"))
+                            .roles("USER")
+                            .build()
+            );
+        }
+
+        if (!userDetailsManager.userExists("admin")) {
+            userDetailsManager.createUser(
+                    User.withUsername("admin")
+                            .password(passwordEncoder().encode("1234"))
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
