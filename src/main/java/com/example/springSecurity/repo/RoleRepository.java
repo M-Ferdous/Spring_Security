ba05f3b388c14ec6809c5c558f9d5cebde9993c5
M-Ferdous <mahzabin.courses@gmail.com>
2025-04-27 12:49:59 +0600

    running

---
+++ src/main/java/com/example/springSecurity/repo/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.springSecurity.repo;
+
+
+import com.example.springSecurity.model.AppRole;
+import com.example.springSecurity.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(AppRole appRole);
+}
